3599dd789418f8c0fedd7991b17fffb22be3cfb9
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2026-05-25 16:41:59 -0300

    feat: funcionalidade adicionar novo anime com API Jikan para capa

--- frontend/src/components/list.tsx
+++ frontend/src/components/list.tsx
@@ -80,6 +80,20 @@ export default function List({
   const [filterCategory, setFilterCategory] = useState<string>("all");
   const [filterValue, setFilterValue] = useState<string>("");
   const [animes, setAnimes] = useState<ListProps[]>(initialAnimes);
+  const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
+  const [isFetchingJikanData, setIsFetchingJikanData] =
+    useState<boolean>(false);
+  const [newAnimeTitle, setNewAnimeTitle] = useState<Partial<ListProps>>({
+    malId: 0,
+    title: "",
+    type: "TV",
+    episodes: 0,
+    watchedEpisodes: 0,
+    score: 0,
+    status: "Plan to Watch",
+    comments: "",
+    imageUrl: "",
+  });
 
   const currentFilterOptions = FILTER_OPTIONS[filterCategory] || [];
 
@@ -158,10 +172,72 @@ export default function List({
     setAnimes(updatedAnimes);
   };
 
+  const fetchJikanData = async () => {
+    if (!newAnimeTitle.malId) return;
+    setIsFetchingJikanData(true);
+    try {
+      const response = await fetch(
+        `https://api.jikan.moe/v4/anime/${newAnimeTitle.malId}`,
+      );
+      if (!response.ok) throw new Error("Anime não encontrado");
+      const { data } = await response.json();
+
+      setNewAnimeTitle((prev) => ({
+        ...prev,
+        title: data.title,
+        imageUrl:
+          data.images?.jpg?.large_image_url || data.images?.jpg?.image_url,
+        episodes: data.episodes || 0,
+        type: data.type || "TV",
+        score: data.score || 0,
+      }));
+    } catch (error) {
+      alert(
+        "Erro ao buscar dados do anime. Verifique o MAL ID e tente novamente.",
+      );
+    } finally {
+      setIsFetchingJikanData(false);
+    }
+  };
+
+  const handleAddAnime = () => {
+    if (!newAnimeTitle.title) {
+      alert("Título do anime é obrigatório.");
+      return;
+    }
+
+    const novoAnime: ListProps = {
+      ...newAnimeTitle,
+      id: Math.floor(Math.random() * 10000),
+      comments: "",
+    } as ListProps;
+
+    setAnimes([novoAnime, ...animes]);
+    setNewAnimeTitle({
+      malId: 0,
+      title: "",
+      type: "TV",
+      episodes: 0,
+      watchedEpisodes: 0,
+      score: 0,
+      status: "Plan to Watch",
+      comments: "",
+      imageUrl: "",
+    });
+    setIsPopupOpen(false);
+  };
+
   return (
     <>
       <div className="d-flex justify-content-between align-items-center mb-4 bg-white p-3 border rounded flex-wrap gap-3">
         <div className="d-flex align-items-center gap-3">
+          <button
+            className="btn btn-primary fw-semibold align-self-start mb-4"
+            onClick={() => setIsPopupOpen(true)}
+          >
+            + Adicionar
+          </button>
+
           <div className="d-flex flex-column align-items-center gap-2">
             <label
               htmlFor="filterCategorySelect"
@@ -368,6 +444,221 @@ export default function List({
           </div>
         )}
       </div>
+      {isPopupOpen && (
+        <div
+          className="modal fade show d-block"
+          tabIndex={-1}
+          style={{
+            backgroundColor: "rgba(0,0,0,0.5)",
+            backdropFilter: "blur(5px)",
+          }}
+        >
+          <div className="modal-dialog modal-dialog-centered modal-lg">
+            <div className="modal-content border-0 shadow-lg">
+              <div className="modal-header border-bottom-0 bg-light pb-0">
+                <h5 className="modal-title fw-semibold text-dark">
+                  Catalogar novo Anime
+                </h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setIsPopupOpen(false)}
+                ></button>
+              </div>
+              <div className="modal-body p-4">
+                <div className="row g-4">
+                  <div className="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center bg-light p-3 border-0">
+                    {newAnimeTitle.imageUrl ? (
+                      <img
+                        src={newAnimeTitle.imageUrl}
+                        alt={newAnimeTitle.title}
+                        className="img-fluid rounded shadow-sm mb-3"
+                        style={{ maxHeight: "250px", objectFit: "contain" }}
+                      />
+                    ) : (
+                      <div
+                        className="bg-secondary d-flex align-items-center justify-content-center p-3 rounded shadow-sm"
+                        style={{
+                          width: "160px",
+                          height: "230px",
+                          opacity: 0.2,
+                        }}
+                      >
+                        <div className="text-center text-white fw-semibold">
+                          Imagem não disponível
+                        </div>
+                      </div>
+                    )}
+                    <label className="form-label text-muted fw-semibold small w-100">
+                      Anime List ID
+                    </label>
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        className="form-control fw-semibold"
+                        placeholder="0000"
+                        value={newAnimeTitle.malId || ""}
+                        onChange={(e) =>
+                          setNewAnimeTitle({
+                            ...newAnimeTitle,
+                            malId: Number(e.target.value),
+                          })
+                        }
+                      />
+                      <button
+                        className="btn btn-dark fw-semibold"
+                        type="button"
+                        onClick={fetchJikanData}
+                        disabled={isFetchingJikanData}
+                      >
+                        {isFetchingJikanData ? "Buscando..." : "Buscar"}
+                      </button>
+                    </div>
+                    <small
+                      className="text-muted mt-2 text-center"
+                      style={{ fontSize: "11px" }}
+                    >
+                      Digite o ID do anime no MyAnimeList para buscar seus
+                      dados.
+                    </small>
+                  </div>
+                  <div className="col-12 col-md-6">
+                    <div className="mb-3">
+                      <label className="form-label text-muted fw-semibold">
+                        Título
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control fw-semibold"
+                        placeholder="Digite o título do anime"
+                        value={newAnimeTitle.title || ""}
+                        onChange={(e) =>
+                          setNewAnimeTitle({
+                            ...newAnimeTitle,
+                            title: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+                    <div className="mb-3 g-2 row">
+                      <div className="col-6">
+                        <label className="form-label text-muted fw-semibold small">
+                          Status
+                        </label>
+                        <select
+                          className="form-select fw-semibold"
+                          value={newAnimeTitle.status}
+                          onChange={(e) =>
+                            setNewAnimeTitle({
+                              ...newAnimeTitle,
+                              status: e.target.value,
+                            })
+                          }
+                        >
+                          {FILTER_OPTIONS.status.map((opt) => (
+                            <option key={opt.value} value={opt.value}>
+                              {opt.label}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                      <div className="col-6">
+                        <label className="form-label text-muted fw-semibold small">
+                          Tipo
+                        </label>
+                        <select
+                          className="form-select fw-semibold"
+                          value={newAnimeTitle.type}
+                          onChange={(e) =>
+                            setNewAnimeTitle({
+                              ...newAnimeTitle,
+                              type: e.target.value,
+                            })
+                          }
+                        >
+                          {FILTER_OPTIONS.type.map((opt) => (
+                            <option key={opt.value} value={opt.value}>
+                              {opt.label}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                      <div className="col-6 mt-3">
+                        <label>Total de episódios</label>
+                        <input
+                          type="number"
+                          className="form-control fw-semibold"
+                          placeholder="Digite o total de episódios"
+                          value={newAnimeTitle.episodes || 0}
+                          onChange={(e) =>
+                            setNewAnimeTitle({
+                              ...newAnimeTitle,
+                              episodes: parseInt(e.target.value) || 0,
+                            })
+                          }
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <label className="form-label text-muted fw-semibold small">
+                        Episódios assistidos
+                      </label>
+                      <input
+                        type="number"
+                        className="form-control fw-semibold"
+                        placeholder="Digite o número de episódios assistidos"
+                        value={newAnimeTitle.watchedEpisodes || 0}
+                        onChange={(e) =>
+                          setNewAnimeTitle({
+                            ...newAnimeTitle,
+                            watchedEpisodes: parseInt(e.target.value) || 0,
+                          })
+                        }
+                      />
+                    </div>
+                    <div>
+                      <label className="form-label text-muted fw-semibold small mt-3">
+                        Nota (0-10)
+                      </label>
+                      <input
+                        type="number"
+                        className="form-control fw-semibold"
+                        placeholder="Digite a nota (0-10)"
+                        value={newAnimeTitle.score || 0}
+                        onChange={(e) =>
+                          setNewAnimeTitle({
+                            ...newAnimeTitle,
+                            score: Math.max(
+                              0,
+                              Math.min(10, parseFloat(e.target.value) || 0),
+                            ),
+                          })
+                        }
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="modal-footer border-top-0 pt-0">
+                <button
+                  type="button"
+                  className="btn btn-primary fw-semibold"
+                  onClick={handleAddAnime}
+                >
+                  Adicionar Anime
+                </button>
+                <button
+                  type="button"
+                  className="btn btn-secondary fw-semibold"
+                  onClick={() => setIsPopupOpen(false)}
+                >
+                  Cancelar
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
